37e7a7b64d29a0ae756ac17d3fe3980b56de68f9
Maarten Paauw <maartenpaauw@gmail.com>
2016-11-21 17:31:37 +0100

    Opdracht 2

--- Week 1/opdracht_2.py
+++ Week 1/opdracht_2.py
@@ -3,3 +3,62 @@ Opdracht 2 - Radialen naar graden
 
 https://dodona.ugent.be/nl/exercises/975017137/
 """
+import math
+
+
+def main() -> None:
+    """
+    Opdracht 2 - Radialen naar graden
+
+    Formule
+    1 radiaal = 180 / 3.1415 = 57.297
+
+    :return: None
+    """
+
+    # invoer in radialen.
+    invoer_radialen = float(input("Voer het aantal radialen in: "))
+
+    # radialen als getal
+    radiaal_als_getal = radiaal_naar_getal(invoer_radialen)
+
+    # Grootte van de hoek in graden
+    grootte_van_de_hoek_in_graden = int(radiaal_als_getal)
+
+    # Uren als een komma getal.
+    uren = float(float(radiaal_als_getal) - int(radiaal_als_getal))
+
+    # Uren en minuten.
+    (uren, minuten) = tijd_berekening(uren)
+
+    # Minuten en seconden.
+    (minuten, seconden) = tijd_berekening(minuten)
+
+    # Print het antwoord in de console.
+    print(int(grootte_van_de_hoek_in_graden), "\n", int(uren), "\n",
+          int(minuten))
+
+
+def tijd_berekening(tijd: float) -> (float, float):
+    """
+    Converteert een float naar een tijdsnotatie als int en
+    het overige als float.
+
+    :param tijd: tijd als float.
+    :return: (float, float)
+    """
+    return divmod(tijd * 60, 1)
+
+
+def radiaal_naar_getal(aantal_radialen: float) -> float:
+    """
+    Coverteer een radiaal naar een komma getal.
+
+    :param aantal_radialen: radiaal als float.
+    :return:                radiaal geconverteerd naar een float.
+    """
+    return (180 / math.pi) * aantal_radialen
+
+
+if __name__ == "__main__":
+    main()
