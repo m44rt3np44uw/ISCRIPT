90a9db93e06023608a0598c0599a714824eedf52
Maarten Paauw <maartenpaauw@gmail.com>
2016-11-26 14:49:11 +0100

    Automatisch de week opdrachten zippen

---
+++ zip.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Week 1
+zip -r s1094220-week-1.zip Week\ 1/
+
+# Week 2
+zip -r s1094220-week-2.zip Week\ 2/
+
+# Week 3
+zip -r s1094220-week-3.zip Week\ 3/
+
+# Week 4
+zip -r s1094220-week-4.zip Week\ 4/
+
+# Week 5
+zip -r s1094220-week-5.zip Week\ 5/
+
+# Week 6
+zip -r s1094220-week-6.zip Week\ 6/
+
+# Week 7
+zip -r s1094220-week-7.zip Week\ 7/
