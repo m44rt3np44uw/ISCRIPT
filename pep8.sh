714e06bbca448e02b41a516b7fa836513c08f69d
Maarten Paauw <maartenpaauw@gmail.com>
2016-12-07 22:17:00 +0100

    PEP8

---
+++ pep8.sh
@@ -0,0 +1,40 @@
+# Week 1
+pep8 Week\ 1/medt_opdracht_1.py
+pep8 Week\ 1/medt_opdracht_2.py
+pep8 Week\ 1/medt_opdracht_3.py
+pep8 Week\ 1/medt_opdracht_4.py
+pep8 Week\ 1/medt_opdracht_5.py
+pep8 Week\ 1/medt_opdracht_6.py
+
+# Week 2
+pep8 Week\ 2/medt_opdracht_1.py
+pep8 Week\ 2/medt_opdracht_2.py
+pep8 Week\ 2/medt_opdracht_3.py
+pep8 Week\ 2/medt_opdracht_4.py
+pep8 Week\ 2/medt_opdracht_5.py
+pep8 Week\ 2/medt_opdracht_6.py
+
+# Week 3
+pep8 Week\ 3/medt_opdracht_1.py
+pep8 Week\ 3/medt_opdracht_2.py
+pep8 Week\ 3/medt_opdracht_3.py
+
+# Week 4
+pep8 Week\ 4/medt_opdracht_1.py
+pep8 Week\ 4/medt_opdracht_2.py
+pep8 Week\ 4/medt_opdracht_3.py
+pep8 Week\ 4/medt_opdracht_4.py
+pep8 Week\ 4/medt_opdracht_5.py
+
+# Week 5
+pep8 Week\ 5/medt_opdracht_1.py
+pep8 Week\ 5/medt_opdracht_2.py
+pep8 Week\ 5/medt_opdracht_3.py
+pep8 Week\ 5/medt_opdracht_4.py
+pep8 Week\ 5/medt_opdracht_5.py
+
+# Week 6
+pep8 Week\ 6/medt_opdracht_1.py
+
+# Week 7
+pep8 Week\ 7/medt_opdracht_1.py
