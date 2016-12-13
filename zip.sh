ecedf7b6ea9a5e196598b7428bae74cadbb022f5
Maarten Paauw <maartenpaauw@gmail.com>
2016-12-13 10:43:36 +0100

    zip script geeft geen feedback meer

--- zip.sh
+++ zip.sh
@@ -7,22 +7,22 @@ rm -rf zip/
 mkdir zip
 
 # Week 1
-zip -r zip/s1094220-medt-week-1.zip Week\ 1/
+zip -rq zip/s1094220-medt-week-1.zip Week\ 1/
 
 # Week 2
-zip -r zip/s1094220-medt-week-2.zip Week\ 2/
+zip -rq zip/s1094220-medt-week-2.zip Week\ 2/
 
 # Week 3
-zip -r zip/s1094220-medt-week-3.zip Week\ 3/
+zip -rq zip/s1094220-medt-week-3.zip Week\ 3/
 
 # Week 4
-zip -r zip/s1094220-medt-week-4.zip Week\ 4/
+zip -rq zip/s1094220-medt-week-4.zip Week\ 4/
 
 # Week 5
-zip -r zip/s1094220-medt-week-5.zip Week\ 5/
+zip -rq zip/s1094220-medt-week-5.zip Week\ 5/
 
 # Week 6
-zip -r zip/s1094220-medt-week-6.zip Week\ 6/
+zip -rq zip/s1094220-medt-week-6.zip Week\ 6/
 
 # Week 7
-zip -r zip/s1094220-medt-week-7.zip Week\ 7/
+zip -rq zip/s1094220-medt-week-7.zip Week\ 7/
